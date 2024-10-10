51c992030af78dd2a0b81e1614263727be7a120d
EduardoRSSilva <jeduardo.rss@gmail.com>
2024-10-10 15:28:34 -0300

    Fontes e Paleta de cores
    Fontes importadas no index.css, e ambas
    configuradas no tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
+import { defaults } from "autoprefixer";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "590C13",
+        },
+        secondary: {
+          DEFAULT: "C5AB87",
+        },
+        offblack: {
+          DEFAULT: "190103",
+        },
+        offwhite: {
+          DEFAULT: "F3F2F2",
+          transparent: "F3F2F226",
+        },
+      },
+
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+        serif: ["Libre Baskerville", "serif"],
+      },
+    },
   },
   plugins: [],
 };
