b5c403e957c30d91f1c8a0af268c4552f1f1b047
EduardoRSSilva <jeduardo.rss@gmail.com>
2024-10-23 19:00:38 -0300

    Update MenuCard.jsx
    Commit pequeno, adicionado imagem ao card

--- src/components/menu/MenuCard.jsx
+++ src/components/menu/MenuCard.jsx
@@ -3,7 +3,7 @@ function MenuCard(props) {
 
   return (
     <article className="flex flex-col gap-2 bg-white rounded-lg shadow-md">
-      
+      <img className="" src={props.itemImageUrl} alt={props.itemImageAlt} />
       <div className="flex flex-col gap-1">
         <h3 className={priceAndTitleStyle}>{props.itemTitle}</h3>
         <p className="text-sm font-light text-offblack">
