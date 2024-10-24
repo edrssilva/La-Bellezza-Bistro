1ac5ce58ec2a481ece3fe3a83a456674c0942125
EduardoRSSilva <jeduardo.rss@gmail.com>
2024-10-23 22:42:58 -0300

    Itens no cardápio
    Progresso na adição de cards

--- src/components/menu/MenuCard.jsx
+++ src/components/menu/MenuCard.jsx
@@ -2,8 +2,12 @@ function MenuCard(props) {
   const priceAndTitleStyle = "text-lg font-medium text-offblack";
 
   return (
-    <article className="flex flex-col gap-2 bg-white rounded-lg shadow-md">
-      <img className="" src={props.itemImageUrl} alt={props.itemImageAlt} />
+    <article className="w-72 h-auto flex flex-col justify-between p-2 bg-white rounded-lg shadow-md hover:scale-[1.05] transition-all cursor-pointer">
+      <img
+        className="rounded w-full h-[196px]"
+        src={props.itemImageUrl}
+        alt={props.itemImageAlt}
+      />
       <div className="flex flex-col gap-1">
         <h3 className={priceAndTitleStyle}>{props.itemTitle}</h3>
         <p className="text-sm font-light text-offblack">
--- src/components/menu/MenuSection.jsx
+++ src/components/menu/MenuSection.jsx
@@ -1,15 +1,103 @@
 import SectionTitle from "../SectionTitle";
 import PrimaryButton from "../PrimaryButton";
+import MenuCard from "./MenuCard";
+import risotoImage from "../../assets/images/dishes/risoto.png";
+import bifeImage from "../../assets/images/dishes/bife.png";
+import ravioliImage from "../../assets/images/dishes/ravioli.png";
+import mignonImage from "../../assets/images/dishes/mignon.png";
+import salmaoImage from "../../assets/images/dishes/salmao.jpg";
+import polentaImage from "../../assets/images/dishes/polenta.jpg";
+import lasanhaImage from "../../assets/images/dishes/lasanha.jpg";
+import patoImage from "../../assets/images/dishes/pato.jpg";
 
 function MenuSection() {
+  const dishes = [
+    {
+      id: 1,
+      title: "Risoto de Camarão",
+      description:
+        "Arroz arbóreo cremoso com camarões frescos, temperado com limão siciliano e um toque de parmesão.",
+      price: "R$69,99",
+      imageUrl: risotoImage,
+    },
+    {
+      id: 2,
+      title: "Bife Ancho ao Molho",
+      description:
+        "Corte especial de bife ancho, servido com molho chimichurri e batatas rústicas.",
+      price: "R$89,99",
+      imageUrl: bifeImage,
+    },
+    {
+      id: 3,
+      title: "Ravióli de Queijo com Molho",
+      description:
+        "Massa fresca recheada com queijos especiais, coberta com molho de tomate caseiro e manjericão.",
+      price: "R$39,99",
+      imageUrl: ravioliImage,
+    },
+    {
+      id: 4,
+      title: "Filé Mignon com Purê",
+      description:
+        "Filé mignon grelhado, acompanhado de purê de batata cremoso e cogumelos salteados.",
+      price: "R$94,99",
+      imageUrl: mignonImage,
+    },
+    {
+      id: 5,
+      title: "Salmão Grelhado com Aspargos",
+      description:
+        "Salmão fresco grelhado, servido com aspargos crocantes e molho de ervas.",
+      price: "R$79,99",
+      imageUrl: salmaoImage,
+    },
+    {
+      id: 6,
+      title: "Polenta Cremosa com Ragu",
+      description:
+        "Polenta cremosa servida com um rico ragu de carne cozida lentamente ao vinho tinto.",
+      price: "R$59,99",
+      imageUrl: polentaImage,
+    },
+    {
+      id: 7,
+      title: "Lasanha de Berinjela",
+      description:
+        "Lasanha leve de berinjela grelhada, intercalada com molho de tomate e queijos selecionados.",
+      price: "R$49,99",
+      imageUrl: lasanhaImage,
+    },
+    {
+      id: 8,
+      title: "Pato ao Molho de Laranja",
+      description:
+        "Pato confit crocante servido com um delicioso molho de laranja e purê de batata-doce.",
+      price: "R$109,99",
+      imageUrl: patoImage,
+    },
+  ];
+
   return (
     <section className="w-2/4 my-16 flex justify-center gap-8">
-      <div className="flex flex-col items-center gap-4">
+      <div className="flex flex-col items-center gap-8">
         <SectionTitle>Cardápio</SectionTitle>
         <h2 className="font-serif font-bold text-2xl text-offblack text-left">
           Mais pedidos
         </h2>
 
+        <div className="grid grid-cols-1 gap-4 xl:grid-cols-2 xl:gap-8 2xl:grid-cols-3">
+          {dishes.map((dish) => (
+            <MenuCard
+              key={dish.id}
+              itemImageUrl={dish.imageUrl}
+              itemImageAlt={dish.title}
+              itemTitle={dish.title}
+              itemDescription={dish.description}
+              itemPrice={dish.price}
+            ></MenuCard>
+          ))}
+        </div>
         <PrimaryButton>Cardápio completo</PrimaryButton>
       </div>
     </section>
