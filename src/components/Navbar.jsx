deed5e9c4027145c3d0f1ea8c175a7aacfd89f63
EduardoRSSilva <jeduardo.rss@gmail.com>
2024-10-24 22:15:59 -0300

    Links e ancoras
    Small fixes, ainda não funcionando

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,33 @@
 import Logo from "./Logo";
 import NavbarItem from "./NavbarItem";
+import { Link } from "react-scroll";
 
 function Navbar() {
   return (
     <nav className="w-full flex justify-between items-center px-16 h-16 bg-primary">
       <Logo color="white" width="auto" height="h-11" />
       <div className="flex gap-16">
-        <NavbarItem to="/">INÍCIO</NavbarItem>
-        <NavbarItem to="about">SOBRE</NavbarItem>
-        <NavbarItem to="menu">CARDÁPIO</NavbarItem>
-        <NavbarItem to="reservation">RESERVAS</NavbarItem>
-        <NavbarItem to="footer">CONTATO</NavbarItem>
+        <Link to="/" spy={true} smooth={true} offset={50} duration={500}>
+          <NavbarItem>INÍCIO</NavbarItem>
+        </Link>
+        <Link to="about" spy={true} smooth={true} offset={50} duration={500}>
+          <NavbarItem>SOBRE</NavbarItem>
+        </Link>
+        <Link to="menu" spy={true} smooth={true} offset={50} duration={500}>
+          <NavbarItem>CARDÁPIO</NavbarItem>
+        </Link>
+        <Link
+          to="reservation"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+        >
+          <NavbarItem>RESERVAS</NavbarItem>
+        </Link>
+        <Link to="footer" spy={true} smooth={true} offset={50} duration={500}>
+          <NavbarItem to="footer">CONTATO</NavbarItem>
+        </Link>
       </div>
     </nav>
   );
--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,17 +1,8 @@
-import { Link } from "react-scroll";
-
 function NavbarItem(props) {
   return (
-    <Link
-      {...props}
-      spy={true}
-      smooth={true}
-      offset={50}
-      duration={500}
-      className="px-4 text-white font-medium cursor-pointer hover:scale-[1.05] origin-center transition-all"
-    >
+    <div className="px-2 text-white font-medium cursor-pointer hover:scale-[1.05] origin-center transition-all">
       {props.children}
-    </Link>
+    </div>
   );
 }
 
