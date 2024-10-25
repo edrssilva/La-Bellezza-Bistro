b636164ecf3ecbb3d54016fb18599059cc0c3495
EduardoRSSilva <jeduardo.rss@gmail.com>
2024-10-24 22:21:40 -0300

    style: Breakpoints
    Definindo breakpoints para responsividade

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      "2xs": "320px",
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1440px",
+      "3xl": "1920px",
+    },
     extend: {
       colors: {
         primary: {
